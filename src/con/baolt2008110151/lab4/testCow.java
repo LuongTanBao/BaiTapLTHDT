9e2c355780dc7e3f94173b1d63b2cf961c5c2020
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:02:11 +0700

    Update testCow.java

--- src/con/baolt2008110151/lab4/testCow.java
+++ src/con/baolt2008110151/lab4/testCow.java
@@ -1,10 +1,20 @@
-package com.baolt2008110151.Lab5;
+package com.baolt2008110151.Lab4;
+import java.util.Scanner;
 
-public class testCow {
-    public static void main(String[] args) {
-        Cow cow = new Cow(0);
-        cow.showData();
-        cow = new Cow(1.100);
-        cow.showData();
+public class Employe {
+    String name;
+    String salary;
+    Scanner scanner = new Scanner(System.in);
+    void getName() {
+        System.out.print("Ho va ten nhan vien : ");
+        name = scanner.nextLine();
+    }
+    void getSalary() {
+        salary = scanner.nextLine();
+        System.out.print("Luong cua nhan vien : ");
+    }
+    void showData() {
+        System.out.println("Ten nhan vien: "+name);
+        System.out.println("Luong cua nhan vien: "+salary);
     }
 }
