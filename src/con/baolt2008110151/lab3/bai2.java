a63ff3659731e1c8bd97c66ba7c19017bf171784
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 21:43:07 +0700

    Create bai2.java

---
+++ src/con/baolt2008110151/lab3/bai2.java
@@ -0,0 +1,14 @@
+package com.baolt2008110151.lap3;
+
+public class lab3bai2 {
+    public static void main(String[] args) {
+        for(int i=2;i<=9;i++){
+            System.out.println();
+            for(int j=1;j<=9;j++){
+                System.out.printf("%d x %d = %d", i, j, i*j);
+                System.out.println();
+            }
+        }
+    }
+    
+}
