fd962b4146d5298973b0a55fb6132e4fd2a5c8dd
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:09:42 +0700

    Create Shape.java

---
+++ src/con/baolt2008110151/tuan6/Shape.java
@@ -0,0 +1,11 @@
+package com.baolt2008110151.tuan6;
+
+public class Shape {
+    private String sound;
+    public void rotate(){
+        System.out.println("xoay 360 độ");
+    }
+    public void playSound(){
+        System.out.println("thêm tệp âm thanh có tên là: "+sound);
+    }
+}
