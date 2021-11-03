e80b7140e14487632d774a69f7a2cb163c8e96dd
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:16:53 +0700

    Create Cat.java

---
+++ src/con/baolt2008110151/tuan7/Cat.java
@@ -0,0 +1,22 @@
+package com.baolt2008110151.tuan7;
+
+public class Cat {
+        void makenoise(){
+            System.out.println("mèo méo meo mèo meo");
+        }
+        void eat(){
+            System.out.println("cat food");
+        }
+        void sleep(){
+            System.out.println("zzz");
+        }
+        void walk(){
+            System.out.println("đi truyền đạo");
+        }
+        void bite(){
+            System.out.println("for fun or chase mouse");
+        }
+        void chase(){
+            System.out.println("chasing the mouse");
+        }
+}
