9794cdad0427586af32f8c1aabe7539a8ab39ab5
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:19:03 +0700

    Create Eagle.java

---
+++ src/con/baolt2008110151/tuan7/Eagle.java
@@ -0,0 +1,19 @@
+package com.baolt2008110151.tuan7;
+
+public class Eagle {
+    void makenoise(){
+        System.out.println("ò ó o ò");
+    }
+    void eat(){
+        System.out.println("dead body, fish,..."); 
+    }
+    void sleep(){
+        System.out.println("zzz"); 
+    }
+    void fly(){
+        System.out.println("hunting");   
+    }
+    void hunting(){
+        System.out.println("fish,turtle"); 
+    }
+}
