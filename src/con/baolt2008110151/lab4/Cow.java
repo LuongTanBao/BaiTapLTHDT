3f1573aa70df36a8e420426992f10b2f1d25163c
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 21:46:52 +0700

    Create Cow.java

---
+++ src/con/baolt2008110151/lab4/Cow.java
@@ -0,0 +1,17 @@
+package com.namnd2008110152.Lab5;
+public class Cow {
+    int age,weight;
+    public Cow(double d) {
+    }
+    Cow(int year,int amount){
+        age = year;
+        weight = amount;
+    }
+    void showData(){
+        System.out.println("Số tuổi: "+age);
+        System.out.println("Cân nặng: "+weight+" Kg");
+    }
+    void Cow(){
+
+    }
+}
