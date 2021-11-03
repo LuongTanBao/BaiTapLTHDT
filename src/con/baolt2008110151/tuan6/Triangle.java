9d6967398b233a6571c7d1eeaac587525bbf44e2
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:11:59 +0700

    Create Triangle.java

---
+++ src/con/baolt2008110151/tuan6/Triangle.java
@@ -0,0 +1,17 @@
+package com.baolt2008110151.tuan6;
+
+public class Triangle extends Shape{
+
+    public Triangle(String string) {
+    }
+
+    public void rotate() {
+        System.out.println("xoay 360 độ");
+    }
+
+    public void playSound() {
+        System.out.println("hình tam giác");
+        System.out.println("thêm âm thanh");
+    }
+
+}
