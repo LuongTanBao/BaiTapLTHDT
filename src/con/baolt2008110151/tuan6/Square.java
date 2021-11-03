5ca3750152a9d7645aebc274226597c9de1d9d6c
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:11:06 +0700

    Create Square.java

---
+++ src/con/baolt2008110151/tuan6/Square.java
@@ -0,0 +1,17 @@
+package com.baolt2008110151.tuan6;
+
+public class Square extends Shape{
+
+    public Square(String string) {
+    }
+
+    public void rotate() {
+        System.out.println("xoay 360 độ");
+    }
+
+    public void playSound() {
+        System.out.println("hình vuông");
+        System.out.println("thêm âm thanh");
+    }
+
+}
