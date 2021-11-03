aaaed441b888ae21b9990a239d446674d16c2268
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:15:49 +0700

    Create Animal.java

---
+++ src/con/baolt2008110151/tuan7/Animal.java
@@ -0,0 +1,30 @@
+package com.baolt2008110151.tuan7;
+
+public class Animal {
+    private String sound;
+    private String picture;
+    private String food;
+    private int hunger;
+    private String boundaries;
+    private int location;
+    protected void makeNoise(){
+        System.out.println("A A A");
+    }
+    protected void eat(){
+        System.out.println("eat ");
+    }
+    protected void sleep(){
+        System.out.println("sleep");
+    }
+    protected void walk(){
+        System.out.println("walk");
+    }
+    protected void bite(){
+        System.out.println("bite");
+    }
+    protected void evolution(){
+        System.out.println("evolution");
+    }
+
+
+}
