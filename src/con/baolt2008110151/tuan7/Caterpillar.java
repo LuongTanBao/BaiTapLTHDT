45f4a03f4eb172798e2e444dd18cab90fc88c749
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:17:29 +0700

    Create Caterpillar.java

---
+++ src/con/baolt2008110151/tuan7/Caterpillar.java
@@ -0,0 +1,16 @@
+package com.baolt2008110151.tuan7;
+
+public class Caterpillar {
+    void eat(){
+        System.out.println("plant");
+    }
+    void sleep(){
+        System.out.println("zzz");
+    }
+    void walk(){
+        System.out.println("finding food");
+    }
+    void evolution(){
+        System.out.println("butterfly");
+    }
+}
