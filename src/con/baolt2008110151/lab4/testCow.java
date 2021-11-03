a3c066e1170465a21fdb3adf1938d6050098b463
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 21:51:29 +0700

    Create testCow.java

---
+++ src/con/baolt2008110151/lab4/testCow.java
@@ -0,0 +1,10 @@
+package com.baolt2008110151.Lab5;
+
+public class testCow {
+    public static void main(String[] args) {
+        Cow cow = new Cow(0);
+        cow.showData();
+        cow = new Cow(1.100);
+        cow.showData();
+    }
+}
