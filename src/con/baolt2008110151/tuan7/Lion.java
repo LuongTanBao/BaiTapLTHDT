c487a057cf0ea5eebfc96b1815e581d9af01fb6e
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:20:16 +0700

    Create Lion.java

---
+++ src/con/baolt2008110151/tuan7/Lion.java
@@ -0,0 +1,22 @@
+package com.baolt2008110151.tuan7;
+
+public class Lion {
+    void makenoise(){
+        System.out.println("groah");
+    }
+    void eat(){
+        System.out.println("meat"); 
+    }
+    void sleep(){
+        System.out.println("zzz"); 
+    }
+    void walk(){
+        System.out.println("look over the herd");   
+    }
+    void hunting(){
+        System.out.println("hunt by crew"); 
+    }
+    void bite(){
+        System.out.println("to fighting with another lion");
+    }
+}
