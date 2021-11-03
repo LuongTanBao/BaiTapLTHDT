99b5d28697f236e8bb253b8a4f1a03e901fa6677
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:18:02 +0700

    Create Dog.java

---
+++ src/con/baolt2008110151/tuan7/Dog.java
@@ -0,0 +1,23 @@
+package com.baolt2008110151.tuan7;
+
+public class Dog {
+    void makenoise(){
+        System.out.println("bark bark...");
+    }
+    void eat(){
+        System.out.println("dog food");
+    }
+    void sleep(){
+        System.out.println("zzz");
+    }
+    void walk(){
+        System.out.println("canh nha");
+    }
+    void bite(){
+        System.out.println("protect the master");
+    }
+    void chase(){
+        System.out.println("chasing the cat");
+    }
+
+}
