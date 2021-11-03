cdb7ef9f408dbcbfc34855ff97a4cf370ea0e064
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:09:07 +0700

    Create Circle.java

---
+++ src/con/baolt2008110151/tuan6/Circle.java
@@ -0,0 +1,17 @@
+package com.baolt2008110151.tuan6;
+
+public class Circle extends Shape {
+
+    public Circle(String string) {
+    }
+
+    public void rotate() {
+        System.out.println("xoay 360 độ");
+    }
+
+    public void playSound() {
+        System.out.println("hình tròn");
+        System.out.println("thêm âm thanh");
+    }
+
+}
