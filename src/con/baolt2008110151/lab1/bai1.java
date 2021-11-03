287fec267251e84991943de78469274ccdffab7c
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 20:55:49 +0700

    Add files via upload

---
+++ src/con/baolt2008110151/lab1/bai1.java
@@ -0,0 +1,15 @@
+package com.bao2008110151.baitap1Lab1;
+import java.until.scanner;
+
+public class LabBai1 {
+        public static void main(string[] args){       
+            Scanner scanner = new scanner(System.in);
+            System.out.print("devil");
+            String hoTen = scanner .nextline();
+            System.out.print("diem TB: ");
+            double diemTB = scanner .nextDouble();
+            System.out.print("devil " +" hello " + "k14");
+            System.out.println();
+             System.out.printf("%s %f diem", hoTen, diemTB);  
+        }
+}
