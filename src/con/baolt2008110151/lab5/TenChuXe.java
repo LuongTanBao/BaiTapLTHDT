ebc04c070753d8b413b283ffec9bb1bb7c7c79f5
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:04:12 +0700

    Create TenChuXe.java

---
+++ src/con/baolt2008110151/lab5/TenChuXe.java
@@ -0,0 +1,23 @@
+package com.baolt2008110151.Lab5;
+public class TenChuXe {
+    String name,address,sex,birth;
+    void getAddress(String text){
+        address = text;
+    }
+    TenChuXe(String text){
+        name = text;
+    }
+    void getSex(String text){
+        sex = text;
+    }
+    void getBirth(String text){
+        birth = text;
+    }
+    void showData(){
+        System.out.println("Tên: "+name);
+        System.out.println("address: "+address);
+        System.out.println("sex(male/female): "+sex);
+        System.out.println("birth: "+birth);
+    }
+}
+© 2021 GitHub, Inc.
