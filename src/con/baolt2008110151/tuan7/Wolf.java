50e1d8b0545d8be25ceddd469e2716ecf1098dd1
LuongTanBao <93199604+LuongTanBao@users.noreply.github.com>
2021-11-03 22:20:51 +0700

    Create Wolf.java

---
+++ src/con/baolt2008110151/tuan7/Wolf.java
@@ -0,0 +1,19 @@
+package com.baolt2008110151.tuan7;
+
+    public class Wolf {
+        void makenoise(){
+            System.out.println("grú");
+        }
+        void eat(){
+            System.out.println("meat"); 
+        }
+        void sleep(){
+            System.out.println("zzz"); 
+        }
+        void hunting(){
+            System.out.println("hunt by crew"); 
+        }
+        void bite(){
+            System.out.println("protect the crew");
+        }
+    }
